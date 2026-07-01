53f65f8056daac1a63b3ee61f3f1fc0342c257c6
Joe Mirarchi <joe@dydx.org>
2026-06-30 21:48:36 -0400

    move strategy smart down the pipeline

--- generate_tiles_pb.py
+++ generate_tiles_pb.py
@@ -101,7 +101,7 @@ def run(cmd):
 
 def extract_region(input_pbf, bbox, output_pbf):
     print(f"\n=== Stage 1: extract region -> {output_pbf} ===")
-    run(["osmium", "extract", "--bbox", bbox, input_pbf, "--output", output_pbf, "--strategy","smart"])
+    run(["osmium", "extract", "--bbox", bbox, "--strategy","smart", input_pbf, "--output", output_pbf])
 
 
 def merge_regions(clips, output_pbf):
