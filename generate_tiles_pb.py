efd91b2a6dc48046bd231ac5f6fad1428e660c39
Joe Mirarchi <joe@dydx.org>
2026-06-30 21:32:56 -0400

    --strategy smart, supposed one-line fix

--- generate_tiles_pb.py
+++ generate_tiles_pb.py
@@ -101,7 +101,7 @@ def run(cmd):
 
 def extract_region(input_pbf, bbox, output_pbf):
     print(f"\n=== Stage 1: extract region -> {output_pbf} ===")
-    run(["osmium", "extract", "--bbox", bbox, input_pbf, "--output", output_pbf])
+    run(["osmium", "extract", "--bbox", bbox, input_pbf, "--output", output_pbf, "--strategy","smart"])
 
 
 def merge_regions(clips, output_pbf):
