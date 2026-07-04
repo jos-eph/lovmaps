16e5f217a1482761c43aa0969259dca895c8e210
jos-eph <85128396+jos-eph@users.noreply.github.com>
2026-07-03 21:56:39 -0400

    Delete test_script.sh

--- test_script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-./generate_tiles_pb.py \
-  --base-name philly_commute_region \
-  --date 2026-05-30T05-50-38Z \
-  --output-dir "$(pwd)" \
-  --bbox -76.00,39.60,-74.60,40.40 \
-  --source-pbf us-northeast-latest.osm.pbf
